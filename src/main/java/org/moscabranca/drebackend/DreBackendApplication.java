7b54cbc05708d1b382eeded19e0709552af33d47
Cauã Sebastian <133614515+Cauasebastian@users.noreply.github.com>
2024-10-25 12:10:52 -0300

    initial commit

---
+++ src/main/java/org/moscabranca/drebackend/DreBackendApplication.java
@@ -0,0 +1,13 @@
+package org.moscabranca.drebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreBackendApplication.class, args);
+    }
+
+}
