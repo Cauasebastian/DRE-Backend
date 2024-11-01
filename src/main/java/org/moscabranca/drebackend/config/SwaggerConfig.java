102d649e6a426f1e649df2cbb04de0a8b7890dcd
Cauã Sebastian <133614515+Cauasebastian@users.noreply.github.com>
2024-11-01 00:15:04 -0300

    Swagger

---
+++ src/main/java/org/moscabranca/drebackend/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package org.moscabranca.drebackend.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("DRE API")
+                        .version("1.0")
+                        .description("API para cálculo de DRE e projeçao de valuation"));
+    }
+}
